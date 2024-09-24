11c11094a39f996b92046107bdf05f0542d31213
HackCat Dev <hello@hackcat.dev>
2024-09-24 17:27:56 +0100

    I forgot to add rollup options in the Vite file, added those in to hopefully get cloudflare running!

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,20 @@
 import { defineConfig } from 'vite';
+import { resolve } from 'path';
 
-export default defineConfig({});
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        character: resolve(__dirname, 'character.html'),
+        nightmarket: resolve(__dirname, 'nightmarket.html'),
+        404: resolve(__dirname, '404.html')
+      },
+      output: {
+        manualChunks: undefined
+      }
+    },
+    outDir: 'dist', // Specify the output directory
+  },
+  publicDir: 'public' // Directory where static assets are located
+});
